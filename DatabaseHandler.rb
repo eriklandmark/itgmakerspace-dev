97b89916cacafbfe1f7365562720afac32e78fb8
itggot-erik-landmark <erik.landmark@itggot.se>
2018-01-30 14:05:22 +0100

    Starting on a custom database handler. Added support for get all, get first, update and delete.

---
+++ DatabaseHandler.rb
@@ -0,0 +1,124 @@
+module DatabaseHandler
+
+  def self.init(db_path:)
+    $db_path = db_path
+    $database = SQLite3::Database.new(db_path)
+  end
+
+  class Table
+    def self.attribute(name, type)
+      @attributes ||= []
+      @attributes << {
+           :name => name,
+           :type => type
+      }
+    end
+
+    def self.table_name(name)
+      @table_name = name
+    end
+
+    def self.get_attributes
+      @attributes
+    end
+
+    def self.get_database_path
+      $db_path
+    end
+
+    def self.get_table_name
+      @table_name
+    end
+
+    def self.execute(str)
+      $database.execute(str)
+    end
+
+    def self.all(condition = {})
+      begin
+        unless condition.empty?
+          con = "WHERE "
+          condition.each_with_index do |key, index|
+            con += "#{key[0]} = #{key[1]}"
+            if index < condition.length - 1
+              con += " and "
+            end
+          end
+        end
+
+        elements = []
+        p "SELECT * FROM #{@table_name} #{con}"
+        $database.execute("SELECT * FROM #{@table_name} #{con}").each do |element|
+          values = {}
+          @attributes.each_with_index do |attribute, index|
+            values[attribute[:name]] = element[index]
+          end
+          elements << DatabaseHandler::DatabaseObject.new(@table_name, values)
+        end
+        elements
+      rescue => e
+        p e
+        nil
+      end
+    end
+
+    def self.first(condition = {})
+      if condition.empty?
+        element = $database.execute("SELECT * FROM #{@table_name} WHERE #{condition.keys.first} = ?", condition[condition.keys.first]).first
+      else
+        element = $database.execute("SELECT * FROM #{@table_name} WHERE #{condition.keys.first} = ?", condition[condition.keys.first]).first
+      end
+
+      if element.nil?
+        nil
+      else
+        values = {}
+        @attributes.each_with_index do |attribute, index|
+          values[attribute[:name]] = element[index]
+        end
+        DatabaseHandler::DatabaseObject.new(@table_name, values)
+      end
+    end
+  end
+
+  class DatabaseObject
+    def initialize(table_name, values)
+      @table_name = table_name
+      values.each do |key, value|
+        self.instance_variable_set("@#{key}".to_sym, value)
+        self.class.send(:attr_accessor, key)
+      end
+
+      def update(elements)
+        sql_str = "UPDATE #{@table_name} SET "
+        values = []
+        elements.each_with_index do |key, index|
+          values << key[1]
+          sql_str += "#{key[0]} = ?"
+          if index < elements.length - 1
+            str += ", "
+          end
+        end
+        sql_str += " WHERE id = ?"
+        values << @id
+        begin
+          $database.execute(sql_str, *values)
+          true
+        rescue => e
+          p e
+          false
+        end
+      end
+
+      def delete
+        begin
+          $database.execute("DELETE FROM #{@table_name} WHERE id = ?", @id)
+          true
+        rescue => e
+          p e
+          false
+        end
+      end
+    end
+  end
+end
---
+++ db_tester.rb
@@ -0,0 +1,31 @@
+require 'sqlite3'
+require 'pp'
+
+require_relative 'DatabaseHandler'
+
+DatabaseHandler.init(db_path: "database/database.sqlite")
+
+class Categories < DatabaseHandler::Table
+  table_name "categories"
+  attribute "id", "Serial"
+  attribute "name", "String"
+end
+
+class Users < DatabaseHandler::Table
+  table_name "users"
+  attribute "id", "Serial"
+  attribute "name", "String"
+end
+
+class Inventory < DatabaseHandler::Table
+  table_name "inventory_items"
+  attribute "id", "Serial"
+  attribute "name", "String"
+  attribute "barcode", "String"
+  attribute "description", "String"
+  attribute "quantity", "Integer"
+  attribute "category", "Integer"
+end
+
+user = Categories.all
+pp user
